4e7c69def8a4d91772db92fbb9a5b382dd5447af
Daniel Kimbel <dkimbel@ginkgobioworks.com>
2021-12-31 11:08:20 -0700

    Update error handling, and add display for ProblemPart

--- src/day1.rs
+++ src/day1.rs
@@ -1,38 +1,41 @@
+use std::error::Error;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
+use crate::ProblemPart;
+
 // TODO(dkimbel): POTENTIAL IMPROVEMENTS
-//   - only panic in `solve`, otherwise use Result
-//   - add some kind of 'display' or 'String::from' impl for super::ProblemPart
 //   - make some trait that lets every day's solution impl solve with an input
-//     file path and a ProblemPart
-//   - find a way to not hardcode `day1` module name into file path, even
-//     if it costs me the use of const?
-//   - have an `Expense` tuplestruct for u32
+//     file path and a ProblemPart. Also make it expose the input file path const/attr?
+//   - refactor ProblemPart into its own file?
+//   - have an `Expense` tuplestruct for u32? type alias?
+//   - try using Anyhow package
+//   - start my own custom rustfmt config file
+//   - make my own Clippy config
+//   - try out the CLion profiler on this code
 //   - create a custom iterator that can take an arbitrary number of input vecs
-//     and return a vec of their combined values; then use that to solve parts one and two
-//     using the same code
+//     and return a vec (or ideally array) of their combined values; then use that to solve
+//     parts one and two using the same code
 
 pub const SAMPLE_FILE_PATH: &str = "day1/resources/sample";
 pub const INPUT_FILE_PATH: &str = "day1/resources/puzzle_inputs";
 
-use crate::ProblemPart;
-
-pub fn solve(problem_part: ProblemPart, file_path: &str) {
+pub fn solve(problem_part: ProblemPart, file_path: &str) -> Result<(), Box<dyn Error>> {
     match problem_part {
         ProblemPart::One => {
-            let analyzer = ExpenseAnalyzer::new(file_path);
-            let (expense_1, expense_2) = analyzer.find_summing_pair(2020);
+            let analyzer = ExpenseAnalyzer::new(file_path)?;
+            let (expense_1, expense_2) = analyzer.find_summing_pair(2020)?;
             let solution = expense_1 * expense_2;
-            println!("{:?} solution: {}", problem_part, solution);
+            println!("{} solution: {}", problem_part, solution);
         }
         ProblemPart::Two => {
-            let analyzer = ExpenseAnalyzer::new(file_path);
-            let (expense_1, expense_2, expense_3) = analyzer.find_summing_triple(2020);
+            let analyzer = ExpenseAnalyzer::new(file_path)?;
+            let (expense_1, expense_2, expense_3) = analyzer.find_summing_triple(2020)?;
             let solution = expense_1 * expense_2 * expense_3;
-            println!("{:?} solution: {}", problem_part, solution);
+            println!("{} solution: {}", problem_part, solution);
         }
     }
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -41,39 +44,38 @@ struct ExpenseAnalyzer {
 }
 
 impl ExpenseAnalyzer {
-    fn new(file_path: &str) -> Self {
-        let file = fs::File::open(file_path).expect("Unable to find input file");
+    fn new(file_path: &str) -> Result<Self, Box<dyn Error>> {
+        let file = fs::File::open(file_path)?;
         let reader = BufReader::new(file);
 
         let mut expenses = Vec::new();
-        for line in reader.lines().map(|r| r.unwrap()) {
-            let expense = line
-                .parse::<u32>()
-                .expect("Could not parse input expense as u32");
+        for line in reader.lines() {
+            let expense: u32 = line?.parse()?;
             expenses.push(expense);
         }
 
-        Self { expenses }
+        Ok(Self { expenses })
     }
 
-    fn find_summing_pair(&self, target_sum: u32) -> (u32, u32) {
+    fn find_summing_pair(&self, target_sum: u32) -> Result<(u32, u32), Box<dyn Error>> {
         let num_expenses = self.expenses.len();
         for i in 0..num_expenses {
             for j in (i + 1)..num_expenses {
                 let expense_i = self.expenses[i];
                 let expense_j = self.expenses[j];
                 if expense_i + expense_j == target_sum {
-                    return (expense_i, expense_j);
+                    return Ok((expense_i, expense_j));
                 }
             }
         }
-        panic!(
+        Err(format!(
             "Could not find a pair of expenses summing to {}",
             target_sum
-        );
+        )
+        .into())
     }
 
-    fn find_summing_triple(&self, target_sum: u32) -> (u32, u32, u32) {
+    fn find_summing_triple(&self, target_sum: u32) -> Result<(u32, u32, u32), Box<dyn Error>> {
         let num_expenses = self.expenses.len();
         for i in 0..num_expenses {
             for j in (i + 1)..num_expenses {
@@ -82,14 +84,11 @@ impl ExpenseAnalyzer {
                     let expense_j = self.expenses[j];
                     let expense_k = self.expenses[k];
                     if expense_i + expense_j + expense_k == target_sum {
-                        return (expense_i, expense_j, expense_k);
+                        return Ok((expense_i, expense_j, expense_k));
                     }
                 }
             }
         }
-        panic!(
-            "Could not find a set of three expenses summing to {}",
-            target_sum
-        );
+        Err(format!("Could not find a set of expenses summing to {}", target_sum).into())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::Formatter;
+
 mod day1;
 
 #[derive(Debug)]
@@ -6,6 +10,16 @@ pub enum ProblemPart {
     Two,
 }
 
-fn main() {
-    day1::solve(ProblemPart::Two, day1::INPUT_FILE_PATH);
+impl fmt::Display for ProblemPart {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let as_int = match self {
+            Self::One => 1,
+            Self::Two => 2,
+        };
+        write!(f, "Part {}", as_int)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    day1::solve(ProblemPart::Two, day1::INPUT_FILE_PATH)
 }
